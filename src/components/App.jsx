f230a4d517dad1f6f6186f331a6326c65a0871ed
Brett Tanonaka <tano.brett@gmail.com>
2023-02-21 17:26:47 -0800

    Enabled VideoList and VideoListEntry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+// import exampleVideoData.js into app
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -7,10 +12,10 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><VideoPlayer video={exampleVideoData[1]}/></h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><VideoList videos={exampleVideoData}/></h5></div>
       </div>
     </div>
   </div>
